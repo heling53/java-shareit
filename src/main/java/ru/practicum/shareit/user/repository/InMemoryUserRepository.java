660e9991f0fdcdeab30fde40eed2b74c735bf7f1
Daniil.Churilov <Heling53@yandex.ru>
2026-03-11 15:41:04 +0300

    InMemory реалиазация

---
+++ src/main/java/ru/practicum/shareit/user/repository/InMemoryUserRepository.java
@@ -0,0 +1,46 @@
+package ru.practicum.shareit.user.repository;
+
+import org.springframework.stereotype.Repository;
+import ru.practicum.shareit.user.model.User;
+
+import java.util.*;
+
+@Repository
+public class InMemoryUserRepository implements UserRepository {
+    private final Map<Long, User> users = new HashMap<>();
+    private long idCounter = 1;
+
+    @Override
+    public User save(User user) {
+        user.setId(idCounter++);
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public User update(User user) {
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return Optional.ofNullable(users.get(id));
+    }
+
+    @Override
+    public List<User> findAll() {
+        return new ArrayList<>(users.values());
+    }
+
+    @Override
+    public void delete(Long id) {
+        users.remove(id);
+    }
+
+    @Override
+    public boolean isEmailExists(String email, Long userId) {
+        return users.values().stream()
+                .anyMatch(u -> u.getEmail().equals(email) && !u.getId().equals(userId));
+    }
+}
