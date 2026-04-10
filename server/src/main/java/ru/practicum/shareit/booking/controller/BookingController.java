4310c238e14e3c8cb9394e298c39492a9e6ac352
Daniil.Churilov <Heling53@yandex.ru>
2026-04-10 12:33:29 +0300

    Тесты

---
+++ server/src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
@@ -0,0 +1,48 @@
+package ru.practicum.shareit.booking.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.booking.dto.BookingDto;
+import ru.practicum.shareit.booking.dto.BookingInputDto;
+import ru.practicum.shareit.booking.service.BookingService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/bookings")
+@RequiredArgsConstructor
+public class BookingController {
+
+    private final BookingService bookingService;
+
+    @PostMapping
+    public BookingDto create(@RequestHeader("X-Sharer-User-Id") Long userId,
+                             @RequestBody BookingInputDto bookingInputDto) {
+        return bookingService.create(userId, bookingInputDto);
+    }
+
+    @PatchMapping("/{bookingId}")
+    public BookingDto approve(@RequestHeader("X-Sharer-User-Id") Long userId,
+                              @PathVariable Long bookingId,
+                              @RequestParam Boolean approved) {
+        return bookingService.approve(userId, bookingId, approved);
+    }
+
+    @GetMapping("/{bookingId}")
+    public BookingDto getById(@RequestHeader("X-Sharer-User-Id") Long userId,
+                              @PathVariable Long bookingId) {
+        return bookingService.getById(userId, bookingId);
+    }
+
+    @GetMapping
+    public List<BookingDto> getAllByBooker(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                           @RequestParam(defaultValue = "ALL") String state) {
+        return bookingService.getAllByBooker(userId, state);
+    }
+
+    @GetMapping("/owner")
+    public List<BookingDto> getAllByOwner(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                          @RequestParam(defaultValue = "ALL") String state) {
+        return bookingService.getAllByOwner(userId, state);
+    }
+}
