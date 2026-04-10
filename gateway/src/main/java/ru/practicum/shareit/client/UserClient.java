7d813839f457dc02ea1cb3b8a522133cb20e6365
Daniil.Churilov <Heling53@yandex.ru>
2026-04-10 12:28:11 +0300

    Тесты

--- gateway/src/main/java/ru/practicum/shareit/client/UserClient.java
+++ gateway/src/main/java/ru/practicum/shareit/client/UserClient.java
@@ -1,45 +1,40 @@
 package ru.practicum.shareit.client;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import ru.practicum.shareit.user.dto.UserDto;
 
 @Service
 public class UserClient extends BaseClient {
+
     private static final String API_PREFIX = "/users";
 
-    @Autowired
     public UserClient(@Value("${shareit-server.url}") String serverUrl, RestTemplateBuilder builder) {
-        super(
-                builder
-                        .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl + API_PREFIX))
-                        .requestFactory(() -> new HttpComponentsClientHttpRequestFactory())
-                        .build()
-        );
+        super(builder
+                .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl + API_PREFIX))
+                .build());
     }
 
     public ResponseEntity<Object> create(UserDto userDto) {
         return post("", userDto);
     }
 
-    public ResponseEntity<Object> update(long userId, UserDto userDto) {
-        return patch("/" + userId, userId, userDto);
-    }
-
     public ResponseEntity<Object> getById(long userId) {
-        return get("/" + userId, userId);
+        return get("/" + userId);
     }
 
     public ResponseEntity<Object> getAll() {
         return get("");
     }
 
+    public ResponseEntity<Object> update(long userId, UserDto userDto) {
+        return patch("/" + userId, userDto);
+    }
+
     public ResponseEntity<Object> deleteUser(long userId) {
-        return delete("/" + userId, userId);
+        return delete("/" + userId);
     }
 }
